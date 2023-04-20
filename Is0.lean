272c6b54da751654bf59f7f5048d2f2882d57935
Malcolm Langfield <35980963+langfield@users.noreply.github.com>
2023-04-20 09:23:21 -0400

    Rename `Literal` -> `Lit`, `Thing` -> `Expression`, `Expression` -> `Op`

--- Is0.lean
+++ Is0.lean
@@ -10,61 +10,63 @@ instance : Fact (Nat.Prime P) := ⟨P_prime⟩
 abbrev Felt := ZMod P
 def Constraint := Prop
 
-inductive Literal where
-  | Val : Felt → Literal
-  | Constraint : Constraint → Literal
+inductive Lit where
+  | Val : Felt → Lit
+  | Constraint : Constraint → Lit
 
-def State := Std.HashMap String Literal
+def State := Std.HashMap String Lit
 
 -- We'll definitely need a context, mapping variable names to values.
 
-inductive Thing : Type → Type where
-  | Literal {a} : a → Thing a
-  | Variable {a} : String → Thing a
+inductive Expression : Type → Type where
+  | Literal {a} : a → Expression a
+  | Variable {a} : String → Expression a
 
-inductive Expression where
-  | Const : Felt → Expression
-  | True : Expression
-  | Get : Vector Felt n → Fin n → Felt → Expression
-  | Set : Vector Felt n → Fin n → Felt → Expression
-  | Sub : Felt → Felt → Expression
-  | Mul : Felt → Felt → Expression
-  | Isz : Felt → Expression
-  | Inv : Felt → Expression
-  | AndEqz : ConstraintThing → FieldThing → Expression
-  | AndCond : Constraint → Felt → Constraint → Expression
-  | Variable : String → Expression
+inductive Op where
+  | Const : Felt → Op
+  | True : Op
+  | Get : Vector Felt n → Fin n → Felt → Op
+  | Set : Vector Felt n → Fin n → Felt → Op
+  | Sub : Felt → Felt → Op
+  | Mul : Felt → Felt → Op
+  | Isz : Felt → Op
+  | Inv : Felt → Op
+  | AndEqz : Expression Constraint → Expression Felt → Op
+  | AndCond : Constraint → Felt → Constraint → Op
+  | Variable : String → Op
 
-def Expression.eval (state : State) (e : Expression) : Literal :=
+def Op.eval (state : State) (e : Op) : Lit :=
   match e with
   | Const x => .Val x
   | True => .Constraint (_root_.True)
   | Get buffer i _ => .Val (buffer.get i)
   | Sub x y => .Val (x - y)
-  | Variable name => state.findD name (Literal.Val 0)
-  | AndEqz c x => .Constraint (c ∧ x = 0)
+  | Variable name => state.findD name (Lit.Val 0)
+  | AndEqz c x => 
+    match c with
+    | (Literal c) => .Constraint (c ∧ x = 0)
   | _ => .Constraint False
 
-def Expression.assign (state : State) (e : Expression) (name : String) : State :=
-  state.insert name (Expression.eval state e)
+def Op.assign (state : State) (e : Op) (name : String) : State :=
+  state.insert name (Op.eval state e)
 
 inductive Cirgen where
   | Sequence : Cirgen → Cirgen → Cirgen
-  | Assign : String → Expression → Cirgen
+  | Assign : String → Op → Cirgen
   -- Should this take a String?
-  | Return : Expression → Cirgen
+  | Return : Op → Cirgen
 
-def Cirgen.step (state : State) (op : Cirgen) : State × Option Literal :=
+def Cirgen.step (state : State) (op : Cirgen) : State × Option Lit :=
   match op with
   -- We may want to encode the index of the instruction in `State`, and then
   -- always assign to the index instead of using a name.
-  | Assign e name => (Expression.assign state e name, none)
+  | Assign e name => (Op.assign state e name, none)
   | Sequence a b => let (state', x) := Cirgen.step state a
                     match x with
                     | some x => (state', some x)
                     | none => Cirgen.step state' b
-  | Return e => (state, Expression.eval state e)
+  | Return e => (state, Op.eval state e)
 
-open Expression Cirgen in
+open Op Cirgen in
 theorem Sub_AndEqz_is_if_is_zero (state : State) :
-  ∀ x : Felt, Sequence (Assign "x-1" (Expression.Sub x 1)) (Assign "(x-1)=0" (AndEqz True (Variable "x-1"))) := by sorry
+  ∀ x : Felt, Sequence (Assign "x-1" (Op.Sub x 1)) (Assign "(x-1)=0" (AndEqz True (Variable "x-1"))) := by sorry
