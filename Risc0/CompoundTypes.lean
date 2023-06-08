983916ea881d01c67e6c9895106ad87d23de422e
Ferinko <silvasi.frantisek@gmail.com>
2023-06-08 15:23:22 +0200

    just don't ask me to prove things about it! ;)

--- Risc0/CompoundTypes.lean
+++ Risc0/CompoundTypes.lean
@@ -9,47 +9,60 @@ namespace Risc0
 --   | Reserve
 --   | Use
 
-inductive MemberType where
-  | StructType: List (String × MemberType) → MemberType
-  | UnionType: List (String × MemberType) → MemberType
-  | ArrayType: MemberType → ℕ → MemberType
-  | Ref      : MemberType
+mutual
 
-def MemberType.GetSize (memberType: MemberType) : ℕ :=
-  match memberType with
-    | StructType fields =>
+inductive ContainerType where
+  | StructType : List (String × MemberType) → ContainerType
+  | UnionType  : List (String × MemberType) → ContainerType
+
+inductive MemberType where
+  | StructType : ContainerType → MemberType
+  | UnionType  : ContainerType → MemberType
+  | ArrayType  : MemberType → ℕ → MemberType
+  | Ref        : MemberType
+
+end
+
+mutual
+
+def ContainerType.size : ContainerType → ℕ
+  | .StructType fields =>
       match fields with
         | List.nil => 0
-        | List.cons (_, fieldType) fs => (GetSize fieldType) + (GetSize (StructType fs))
-    | UnionType fields       =>
+        | List.cons  (_, fieldType) fs => fieldType.size + (ContainerType.StructType fs).size
+  | .UnionType fields =>
       match fields with
-        | List.nil                       => 0
-        | List.cons (_, fieldType) fs => max (GetSize fieldType) (GetSize (UnionType fs))
-    | ArrayType elemType len => (GetSize elemType) * len
-    | Ref                    => 1
+        | List.nil => 0
+        | List.cons (_, fieldType) fs => max fieldType.size (ContainerType.UnionType fs).size
 
-def MemberType.GetFieldOffsetSize (memberType: MemberType) (name: String) : Option (ℕ × ℕ) :=
+def MemberType.size (memberType: MemberType) : ℕ :=
   match memberType with
-    | StructType fields      =>
-      match fields with
-        | List.nil => .none
-        | List.cons (fieldName, fieldType) fs =>
-          if name = fieldName
-          then .some (0, GetSize fieldType)
-          else
-            let x := GetFieldOffsetSize (StructType fs) name
-            if x.isNone
-            then .none
-            else .some ((GetSize fieldType) + x.get!.fst,x.get!.snd)
-    | UnionType fields       =>
-      match fields with
-        | List.nil => .none
-        | List.cons (fieldName, fieldType) fs =>
-          if name = fieldName
-          then .some (0, GetSize fieldType)
-          else GetFieldOffsetSize (UnionType fs) name
-    | ArrayType _ _ => .none
-    | Ref           => .none
+    | .StructType c | .UnionType c => c.size
+    | .ArrayType c n               => c.size * n
+    | .Ref                         => 1
+
+end
+
+def ContainerType.offsetSize (c : ContainerType) (name : String) : Option (ℕ × ℕ) :=
+  match c with
+    | StructType fields =>
+        match fields with
+          | List.nil => .none
+          | List.cons (fieldName, fieldType) fs =>
+              if name = fieldName
+              then .some (0, fieldType.size)
+              else
+                let x := (StructType fs).offsetSize name
+                if x.isNone
+                then .none
+                else .some (fieldType.size + x.get!.fst, x.get!.snd)
+    | UnionType fields =>
+        match fields with
+          | List.nil => .none
+          | List.cons (fieldName, fieldType) fs =>
+              if name = fieldName
+              then .some (0, fieldType.size)
+              else (UnionType fs).offsetSize name
 
 namespace CompoundOp
 
@@ -61,14 +74,14 @@ def Load {isNondet: IsNondet} (ref: BufferVar): Op isNondet := Op.Get ref 0 0
 
 def Store (ref: BufferVar) (val: FeltVar) := MLIR.Set ref 0 val
 
-def Lookup {isNondet: IsNondet} (container: BufferVar) (containerType: MemberType) (fieldName: String): Op isNondet :=
-  let (offset, size) := (containerType.GetFieldOffsetSize fieldName).get!
+def Lookup {isNondet: IsNondet} (container: BufferVar) (containerType: ContainerType) (fieldName: String): Op isNondet :=
+  let (offset, size) := (containerType.offsetSize fieldName).get!
   Op.Slice container offset size
 
-def Subscript {isNondet : IsNondet} (container: BufferVar) (containerType: MemberType) (idx: ℕ): Option (Op isNondet) :=
+def Subscript {isNondet : IsNondet} (container: BufferVar) (containerType: MemberType) (idx: ℕ) : Option (Op isNondet) :=
   match containerType with
     | MemberType.ArrayType elemType _ =>
-      .some (Op.Slice container (idx * elemType.GetSize) elemType.GetSize)
+      .some (Op.Slice container (idx * elemType.size) elemType.size)
     | _ => .none
 
 end CompoundOp
