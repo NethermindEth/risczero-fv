bb159b97572c44d19aaeae20c110c625da2cf19e
Dom Henderson <dominic@nethermind.io>
2023-06-13 15:20:45 +0100

    WIP

--- Risc0/Gadgets/IsZero/Axioms.lean
+++ Risc0/Gadgets/IsZero/Axioms.lean
@@ -8,6 +8,7 @@ import Mathlib.Tactic.FieldSimp
 import Risc0.Basic
 import Risc0.Lemmas
 import Risc0.Wheels
+import Risc0.MlirTactics
 
 namespace Risc0
 
@@ -103,44 +104,6 @@ def is0_constraints (input : Felt) (output : List Felt) : Prop :=
     -- %11 = cirgen.and_cond %5, %6 : <default>, %10
     "if other cond" ←ₐ guard ⟨"1 - out[0]"⟩ & ⟨"if out[0] then eqz x"⟩ with ⟨"other cond"⟩ 
   (state'.props ⟨"if other cond"⟩).getD True
-      
-section tactics
-
-open Lean Elab Tactic
-
-open Op in
-elab "simp_op" : tactic => do
-  evalTactic <| ← `( tactic|
-    simp only [
-      eval_const, eval_true, eval_getBuffer, eval_sub,
-      eval_mul, eval_isz, eval_inv, eval_andEqz, eval_andCond
-    ]
-  )
-
-elab "MLIR" : tactic => do
-  evalTactic <| ← `(
-    tactic| repeat ( first |
-      rw [MLIR.run_ass_def] | rw [MLIR.run_set_output] | rw [MLIR.run_if] |
-      rw [MLIR.run_nondet] | rw [MLIR.run_eqz] |
-      rw [MLIR.run_seq_def] 
-      all_goals try rfl
-      simp_op
-    )
-  )
-  evalTactic <| ← `(tactic| try rw [MLIR.run_ass_def])
-  evalTactic <| ← `(tactic| simp)
-
-elab "MLIR_state" : tactic => do
-  evalTactic <| ← `(tactic| repeat rw [Map.update_get_skip])
-  evalTactic <| ← `(tactic| all_goals try decide)
-  evalTactic <| ← `(tactic| all_goals try rfl)
-  evalTactic <| ← `(tactic| all_goals simp only)
-  -- evalTactic <| ← `(tactic| simp)
-
-elab "MLIR_states" : tactic => do
-  evalTactic <| ← `(tactic| repeat MLIR_state)
-
-end tactics
   
 set_option maxHeartbeats 2000000 in
 lemma is0_constraints_closed_form {x y₁ y₂ : Felt} :
