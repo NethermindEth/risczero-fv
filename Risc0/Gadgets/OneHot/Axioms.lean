357435045ed1ddeb9a9f68323056007a477c222e
ElijahVlasov <elijah.vlasov@gmail.com>
2023-06-16 15:53:13 +0100

    Add parametric witness generator for OneHot

--- Risc0/Gadgets/OneHot/Axioms.lean
+++ Risc0/Gadgets/OneHot/Axioms.lean
@@ -112,83 +112,73 @@ def witness_prog_full : MLIRProgram :=
   "outputSum - 1" ←ₐ .Sub ⟨"outputSum"⟩ ⟨"1"⟩;
   ?₀ ⟨"outputSum - 1"⟩
 
-def witness_prog_0_setup : MLIRProgram :=
+def witness_prog_0_setup (n : ℕ) : MLIRProgram :=
  -- %0 = cirgen.const 2
-  "2" ←ₐ .Const 2;
-   -- %1 = cirgen.const 1
-  "1" ←ₐ .Const 1;
-   -- %2 = cirgen.const 0
-  "0" ←ₐ .Const 0;
+  (match n with
+  | Nat.succ n => 
+      toString (Nat.succ n) ←ₐ .Const (Nat.succ n); 
+      witness_prog_0_setup n
+  | Nat.zero => "0" ←ₐ .Const 0);
    -- %3 = cirgen.get %arg0[0] back 0 : <1, constant>
   "input" ←ₐ .Get ⟨"input"⟩ 0 0
 
-def witness_prog_1_nondet : MLIRProgram :=
+
+def witness_prog_1_nondet_inner (n : ℕ) : MLIR IsNondet.InNondet :=
    -- cirgen.nondet {
-  nondet (
      --   %19 = cirgen.isz %3 : <default>
-    "input == 0" ←ₐ ??₀⟨"input"⟩;
-     --   cirgen.set %arg1 : <3, mutable>[0] = %19 : <default>
-    ⟨"output"⟩[0] ←ᵢ ⟨"input == 0"⟩;
-     --   %20 = cirgen.sub %3 : <default>, %1 : <default>
-    "input - 1" ←ₐ .Sub ⟨"input"⟩ ⟨"1"⟩;
-     --   %21 = cirgen.isz %20 : <default>
-    "input == 1" ←ₐ ??₀⟨"input - 1"⟩;
-     --   cirgen.set %arg1 : <3, mutable>[1] = %21 : <default>
-    ⟨"output"⟩[1] ←ᵢ ⟨"input == 1"⟩;
-     --   %22 = cirgen.sub %3 : <default>, %0 : <default>
-    "input - 2" ←ₐ .Sub ⟨"input"⟩ ⟨"2"⟩;
-     --   %23 = cirgen.isz %22 : <default>
-    "input == 2" ←ₐ ??₀⟨"input - 2"⟩;
-     --   cirgen.set %arg1 : <3, mutable>[2] = %23 : <default>
-    ⟨"output"⟩[2] ←ᵢ ⟨"input == 2"⟩
-     -- }
-  )
+    match n with
+    | Nat.succ n =>
+      witness_prog_1_nondet_inner n;
+      "input - " ++ (toString (Nat.succ n)) ←ₐ .Sub ⟨"input"⟩ ⟨toString (Nat.succ n)⟩;
+      "input == " ++ (toString (Nat.succ n)) ←ₐ ??₀⟨"input"⟩;
+      ⟨"output"⟩[Nat.succ n] ←ᵢ ⟨"input == " ++ (toString (Nat.succ n))⟩
+    | Nat.zero =>     
+      "input == 0" ←ₐ ??₀⟨"input"⟩;
+      --   cirgen.set %arg1 : <3, mutable>[0] = %19 : <default>
+      ⟨"output"⟩[0] ←ᵢ ⟨"input == 0"⟩
 
-def witness_prog_2_projection : MLIRProgram :=
-   -- %4 = cirgen.get %arg1[1] back 0 : <3, mutable>
-  "output[1]" ←ₐ .Get ⟨"output"⟩ 0 1;
-   -- %5 = cirgen.get %arg1[2] back 0 : <3, mutable>
-  "output[2]" ←ₐ .Get ⟨"output"⟩ 0 2;
-   -- %6 = cirgen.mul %5 : <default>, %0 : <default>
-  "output[2] * 2" ←ₐ .Mul ⟨"output[2]"⟩ ⟨"2"⟩;
-   -- %7 = cirgen.add %4 : <default>, %6 : <default>
-  "2*output[2]+output[1]" ←ₐ .Add ⟨"output[1]"⟩ ⟨"output[2] * 2"⟩;
-   -- %8 = cirgen.sub %7 : <default>, %3 : <default>
-  "2*output[2]+output[1] - input" ←ₐ .Sub ⟨"2*output[2]+output[1]"⟩ ⟨"input"⟩;
-   -- cirgen.eqz %8 : <default>
-  ?₀ ⟨"2*output[2]+output[1] - input"⟩
+def witness_prog_1nondet (n : ℕ) : MLIRProgram := MLIR.Nondet (witness_prog_1_nondet_inner n)
 
-def witness_prog_3_output0_le_1 : MLIRProgram :=
+def witness_prog_2_projection (n : ℕ) : MLIRProgram :=
+  match n with
+  | (Nat.succ Nat.zero) => "output[1]" ←ₐ .Get ⟨"output"⟩ 0 1 
+  | Nat.succ n => 
+      witness_prog_2_projection n;
+      "output[" ++ toString n.succ ++ "]" ←ₐ .Get ⟨"output"⟩ 0 n.succ;
+      "output[" ++ toString n.succ ++ "] * " ++ toString n.succ ←ₐ .Mul ⟨"output[" ++ toString n.succ ++ "]"⟩ ⟨toString n.succ⟩;
+      "sum" ++ toString n.succ ←ₐ .Add ⟨"output[" ++ toString n ++ "]"⟩ ⟨"output[" ++ toString n.succ ++ "] * " ++ toString n.succ⟩
+  | Nat.zero => "RIP" ←ₐ .Const 42
+
+def witness_prog_3_sum_equals_input (n : ℕ) : MLIRProgram :=
+  "sum" ++ toString n ++ " - input" ←ₐ .Sub ⟨"sum" ++ toString n⟩ ⟨"input"⟩;
+  ?₀ ⟨"sum" ++ toString n ++ " - input"⟩
+
+def witness_prog_4_output0_le_1_and_sum (n : ℕ) : MLIRProgram :=
    -- %9 = cirgen.get %arg1[0] back 0 : <3, mutable>
-  "output[0]" ←ₐ .Get ⟨"output"⟩ 0 0;
-   -- %10 = cirgen.sub %1 : <default>, %9 : <default>
-  "1 - Output[0]" ←ₐ .Sub ⟨"1"⟩ ⟨"output[0]"⟩;
-   -- %11 = cirgen.mul %9 : <default>, %10 : <default>
-  "output[0] <= 1" ←ₐ .Mul ⟨"output[0]"⟩ ⟨"1 - Output[0]"⟩;
-   -- cirgen.eqz %11 : <default>
-  ?₀ ⟨"output[0] <= 1"⟩
+  match n with
+  | Nat.succ n => 
+    witness_prog_4_output0_le_1_and_sum n;
+    -- %10 = cirgen.sub %1 : <default>, %9 : <default>
+    "1 - output[" ++ toString n.succ ++ "]" ←ₐ .Sub ⟨"1"⟩ ⟨"output[" ++ toString n.succ ++ "]"⟩;
+    -- %11 = cirgen.mul %9 : <default>, %10 : <default>
+    "output[" ++ toString n.succ ++ "] <= 1" ←ₐ .Mul ⟨"output[" ++ toString n.succ ++ "]"⟩ ⟨"1 - Output[" ++ toString n.succ ++ "]"⟩;
+    -- cirgen.eqz %11 : <default>
+    ?₀ ⟨"output[" ++ toString n.succ ++ "] <= 1"⟩;
+    "output_sum[" ++ toString n.succ ++ "]" ←ₐ .Add ⟨"output_sum[" ++ toString n ++ "]"⟩ ⟨"output[" ++ toString n.succ ++ "]"⟩
+  | Nat.zero => 
+    "output_sum[0]" ←ₐ .Get ⟨"output"⟩ 0 0;
+    -- %10 = cirgen.sub %1 : <default>, %9 : <default>
+    "1 - output[0]" ←ₐ .Sub ⟨"1"⟩ ⟨"output[0]"⟩;
+    -- %11 = cirgen.mul %9 : <default>, %10 : <default>
+    "output[0] <= 1" ←ₐ .Mul ⟨"output[0]"⟩ ⟨"1 - output[0]"⟩;
+    -- cirgen.eqz %11 : <default>
+    ?₀ ⟨"output[0] <= 1"⟩
 
-def witness_prog_3_output1_le_1 : MLIRProgram :=
-   -- %12 = cirgen.sub %1 : <default>, %4 : <default>
-  "1 - Output[1]" ←ₐ .Sub ⟨"1"⟩ ⟨"output[1]"⟩;
-   -- %13 = cirgen.mul %4 : <default>, %12 : <default>
-  "output[1] <= 1" ←ₐ .Mul ⟨"output[1]"⟩ ⟨"1 - Output[1]"⟩;
-   -- cirgen.eqz %13 : <default>
-  ?₀ ⟨"output[1] <= 1"⟩
 
-def witness_prog_4_final_constraints : MLIRProgram :=
-   -- %14 = cirgen.add %9 : <default>, %4 : <default>
-  "output[0]AddOutput[1]" ←ₐ .Add ⟨"output[0]"⟩ ⟨"output[1]"⟩;
-   -- %15 = cirgen.sub %1 : <default>, %5 : <default>
-  "1 - Output[2]" ←ₐ .Sub ⟨"1"⟩ ⟨"output[2]"⟩;
-   -- %16 = cirgen.mul %5 : <default>, %15 : <default>
-  "output[2] <= 1" ←ₐ .Mul ⟨"output[2]"⟩ ⟨"1 - Output[2]"⟩;
-   -- cirgen.eqz %16 : <default>
-  ?₀ ⟨"output[2] <= 1"⟩;
+def witness_prog_4_final_constraints (n : ℕ) : MLIRProgram :=
    -- %17 = cirgen.add %14 : <default>, %5 : <default>
-  "outputSum" ←ₐ .Add ⟨"output[0]AddOutput[1]"⟩ ⟨"output[2]"⟩;
    -- %18 = cirgen.sub %17 : <default>, %1 : <default>
-  "outputSum - 1" ←ₐ .Sub ⟨"outputSum"⟩ ⟨"1"⟩;
+  "outputSum - 1" ←ₐ .Sub ⟨"output_sum[" ++ toString n ++ "]"⟩ ⟨"1"⟩;
   --     cirgen.eqz %18 : <default>
   ?₀ ⟨"outputSum - 1"⟩
 
