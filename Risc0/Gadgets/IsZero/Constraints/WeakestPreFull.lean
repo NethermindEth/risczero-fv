c32b5810845be74baa2fc6a1f37cb3fa4f84e27e
Dom Henderson <dominic@nethermind.io>
2023-06-22 21:42:49 +0100

    WIP IsZero.constraints_of_spec

--- Risc0/Gadgets/IsZero/Constraints/WeakestPreFull.lean
+++ Risc0/Gadgets/IsZero/Constraints/WeakestPreFull.lean
@@ -1,3 +1,4 @@
+import Risc0.Basic
 import Risc0.Gadgets.IsZero.Constraints.Code
 import Risc0.Gadgets.IsZero.Constraints.WeakestPresPart0
 import Risc0.Gadgets.IsZero.Constraints.WeakestPresPart1
@@ -97,89 +98,153 @@ lemma wellFormed_validGet {st: State}
 -- Temporary lemma used to work out what will be needed once wellFormed_validGet is working
 lemma isGetValidSkip : isGetValid st buf back offset := sorry
 
-lemma closed_form {st: State} {x: Felt} {y₁ y₂ : Option Felt} : start_state st x y₁ y₂ → (
-    Code.run st ↔
-    ((if Option.get! y₁ = 0 then True else x = 0) ∧ if 1 - Option.get! y₁ = 0 then True else x * Option.get! y₂ - 1 = 0)
-  ) := by
-  unfold start_state
-  intro h_pre
-  rw [part₀_wp]
-  rw [part₁_updates_opaque]
-  rw [part₂_updates_opaque]
-  rw [part₃_updates_opaque]
-  rw [part₄_updates_opaque]
+-- lemma closed_form {st: State} {x: Felt} {y₁ y₂ : Option Felt} : start_state st x y₁ y₂ → (
+--     Code.run st ↔
+--     ((if Option.get! y₁ = 0 then True else x = 0) ∧ if 1 - Option.get! y₁ = 0 then True else x * Option.get! y₂ - 1 = 0)
+--   ) := by
+--   unfold start_state
+--   intro h_pre
+--   rw [part₀_wp]
+--   rw [part₁_updates_opaque]
+--   rw [part₂_updates_opaque]
+--   rw [part₃_updates_opaque]
+--   rw [part₄_updates_opaque]
 
-  generalize eq : ((if Option.get! y₁ = 0 then True else x = 0) ∧ if 1 - Option.get! y₁ = 0 then True else x * Option.get! y₂ - 1 = 0) = rhs
+--   generalize eq : ((if Option.get! y₁ = 0 then True else x = 0) ∧ if 1 - Option.get! y₁ = 0 then True else x * Option.get! y₂ - 1 = 0) = rhs
 
-  unfold part₀_state
-  MLIR_states_updates
+--   unfold part₀_state
+--   MLIR_states_updates
 
-  unfold part₁_state
-  -- have hwf: st.WellFormed := by simp [h_pre]
-  -- have hvar: { name := "input" } ∈ st.vars := by simp only [h_pre]
-  -- have hwidth: st.bufferWidths[({ name := "input" }: BufferVar)] = some 1 := by simp only [h_pre]
-  -- have htime: 0 ≤ st.cycle := by simp only [zero_le]
-  -- have hdata: (0 < 1) := by simp only
-  -- have helem: Buffer.get! (Option.get! st.buffers[({name := "input"}: BufferVar)]) (st.cycle, 0) = some x :=
-  --   by simp? [h_pre, Buffer.get!, Buffer.Idx.time, Buffer.Idx.data] -- requires (List.get! st.buffers[({name := "input"}: BufferVar)].get! st.cycle = [some input]) in the start_state
-  -- have hvalidget: isGetValid st { name := "input" } 0 0 :=
-  --   wellFormed_validGet hwf hvar hwidth htime hdata helem
-    -- REVIEW: why does this work, but adding wellFormed_validGet, hwf, hvar, hwidth, htime, hdata, helem to the simp does not?
-  simp? [
-    h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
-    State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
-    Map.update_get_skip, Map.update_get_next
-  ]
-  -- NOTE: place cursor here for maximally simple goal, the follow up simps complicate things
-  -- simp? [
-  --   getImpl, isGetValid, Buffer.back, Option.get!,
-  --   Buffer.get!, State.set!, State.setBufferElementImpl, State.set!, Buffer.set?,
-  --   Option.isEqSome, List.set, State.felts_if, State.buffers_if, State.bufferWidths_if,
-  --   State.cycle_if, State.isFailed_if, State.vars_if, State.props_if,
-  --   State.constraints_if, State.props_if
-  -- ] --states_new
-  -- simp? [
-  --   Map.update, getElem!, ite_true, Option.get!_of_some, ite_false, true_and, Option.getD_some,
-  --   Map.fromList_cons, Map.fromList_nil, State.update_val', 
-  --   le_refl, List.find?, List.mem_cons, ge_iff_le, tsub_eq_zero_of_le,
-  --   List.cons.injEq, and_imp, forall_apply_eq_imp_iff', forall_eq',
-  --   Nat.succ_ne_self, IsEmpty.forall_iff, implies_true, forall_const, Nat.succ.injEq,
-  --   getElem, instGetElemMapOptionTrue, State.updateFelts_buffers, State.updateFelts_bufferWidths,
-  --   State.updateFelts_constraints, State.updateFelts_cycle, State.updateFelts_isFailed,
-  --   State.updateFelts_props, State.updateFelts_vars, State.updateFelts_felts, State.updateProps_buffers,
-  --   State.updateProps_bufferWidths, State.updateProps_constraints, State.updateProps_cycle,
-  --   State.updateProps_isFailed, State.updateProps_felts, State.updateProps_vars, State.updateProps_props,
-  --   Buffer.Idx.time, Buffer.Idx.data, Back.toNat, h_pre, nonpos_iff_eq_zero, tsub_zero
-  -- ] --decide_updates
-  -- simp only [Map.update_def.symm]
+--   unfold part₁_state
+--   -- have hwf: st.WellFormed := by simp [h_pre]
+--   -- have hvar: { name := "input" } ∈ st.vars := by simp only [h_pre]
+--   -- have hwidth: st.bufferWidths[({ name := "input" }: BufferVar)] = some 1 := by simp only [h_pre]
+--   -- have htime: 0 ≤ st.cycle := by simp only [zero_le]
+--   -- have hdata: (0 < 1) := by simp only
+--   -- have helem: Buffer.get! (Option.get! st.buffers[({name := "input"}: BufferVar)]) (st.cycle, 0) = some x :=
+--   --   by simp? [h_pre, Buffer.get!, Buffer.Idx.time, Buffer.Idx.data] -- requires (List.get! st.buffers[({name := "input"}: BufferVar)].get! st.cycle = [some input]) in the start_state
+--   -- have hvalidget: isGetValid st { name := "input" } 0 0 :=
+--   --   wellFormed_validGet hwf hvar hwidth htime hdata helem
+--     -- REVIEW: why does this work, but adding wellFormed_validGet, hwf, hvar, hwidth, htime, hdata, helem to the simp does not?
+--   simp? [
+--     h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+--     State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+--     Map.update_get_skip, Map.update_get_next
+--   ]
+--   -- NOTE: place cursor here for maximally simple goal, the follow up simps complicate things
+--   -- simp? [
+--   --   getImpl, isGetValid, Buffer.back, Option.get!,
+--   --   Buffer.get!, State.set!, State.setBufferElementImpl, State.set!, Buffer.set?,
+--   --   Option.isEqSome, List.set, State.felts_if, State.buffers_if, State.bufferWidths_if,
+--   --   State.cycle_if, State.isFailed_if, State.vars_if, State.props_if,
+--   --   State.constraints_if, State.props_if
+--   -- ] --states_new
+--   -- simp? [
+--   --   Map.update, getElem!, ite_true, Option.get!_of_some, ite_false, true_and, Option.getD_some,
+--   --   Map.fromList_cons, Map.fromList_nil, State.update_val', 
+--   --   le_refl, List.find?, List.mem_cons, ge_iff_le, tsub_eq_zero_of_le,
+--   --   List.cons.injEq, and_imp, forall_apply_eq_imp_iff', forall_eq',
+--   --   Nat.succ_ne_self, IsEmpty.forall_iff, implies_true, forall_const, Nat.succ.injEq,
+--   --   getElem, instGetElemMapOptionTrue, State.updateFelts_buffers, State.updateFelts_bufferWidths,
+--   --   State.updateFelts_constraints, State.updateFelts_cycle, State.updateFelts_isFailed,
+--   --   State.updateFelts_props, State.updateFelts_vars, State.updateFelts_felts, State.updateProps_buffers,
+--   --   State.updateProps_bufferWidths, State.updateProps_constraints, State.updateProps_cycle,
+--   --   State.updateProps_isFailed, State.updateProps_felts, State.updateProps_vars, State.updateProps_props,
+--   --   Buffer.Idx.time, Buffer.Idx.data, Back.toNat, h_pre, nonpos_iff_eq_zero, tsub_zero
+--   -- ] --decide_updates
+--   -- simp only [Map.update_def.symm]
   
-  unfold part₂_state
-  simp? [
-    h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
-    State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
-    Map.update_get_skip, Map.update_get_next
-  ]
-  -- MLIR_states_updates
+--   unfold part₂_state
+--   simp? [
+--     h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+--     State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+--     Map.update_get_skip, Map.update_get_next
+--   ]
+--   -- MLIR_states_updates
 
-  unfold part₃_state
-  simp? [
-    h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
-    State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
-    Map.update_get_skip, Map.update_get_next
-  ]
+--   unfold part₃_state
+--   simp? [
+--     h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+--     State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+--     Map.update_get_skip, Map.update_get_next
+--   ]
 
-  -- MLIR_states_updates
+--   -- MLIR_states_updates
 
-  unfold part₄_state
-  simp? [
-    h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
-    State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
-    Map.update_get_skip, Map.update_get_next
-  ]
-  -- MLIR_states_updates
+--   unfold part₄_state
+--   simp? [
+--     h_pre, getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+--     State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+--     Map.update_get_skip, Map.update_get_next
+--   ]
+--   -- MLIR_states_updates
 
-  simp [Code.getReturn, State.constraintsInVar, State.constraints]
-  rw [←eq]
+--   simp [Code.getReturn, State.constraintsInVar, State.constraints]
+--   rw [←eq]
+
+def buffersPopulated (st: State) (input is0 inv: Felt) : Prop :=
+  Buffer.back st ⟨"input"⟩ 0 0 = some input ∧
+  Buffer.back st ⟨"output"⟩ 0 0 = some is0 ∧
+  Buffer.back st ⟨"output"⟩ 0 1 = some inv
+
+theorem constraints_of_spec : st.Valid → (Code.run st ↔ (buffersPopulated st input is0 inv ∧ (input = 0 ∧ is0 = 1 ∨ input ≠ 0 ∧ is0 = 0 ∧ inv = input⁻¹))) := by
+  intro h_valid
+  apply Iff.intro
+  case mpr =>
+    show buffersPopulated st input is0 inv ∧ (input = 0 ∧ is0 = 1 ∨ input ≠ 0 ∧ is0 = 0 ∧ inv = input⁻¹) → Code.run st
+    unfold buffersPopulated
+    intro h_pre
+    rw [part₀_wp]
+    rw [part₁_updates_opaque]
+    rw [part₂_updates_opaque]
+    rw [part₃_updates_opaque]
+    rw [part₄_updates_opaque]
+
+    unfold part₀_state
+    MLIR_states_updates
+
+    unfold part₁_state
+    simp? [
+      getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+      State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+      Map.update_get_skip, Map.update_get_next
+    ]
+    rewrite [h_pre.left.left]
+    rewrite [h_pre.left.right.left]
+    simp only [Option.get!_of_some]
+
+    unfold part₂_state
+    simp? [
+      getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+      State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+      Map.update_get_skip, Map.update_get_next
+    ]
+
+    unfold part₃_state
+    simp? [
+      getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+      State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+      Map.update_get_skip, Map.update_get_next
+    ]
+    rewrite [h_pre.left.right.right]
+    simp only [Option.get!_of_some]
+
+    unfold part₄_state
+    simp? [
+      getImpl, State.updateFelts_isGetValid, State.updateProps_isGetValid, isGetValidSkip,
+      State.updateFelts_bufferBack, State.updateProps_bufferBack, Map.update_get_skip', Map.update_get_next',
+      Map.update_get_skip, Map.update_get_next
+    ]
+
+    unfold Code.getReturn
+    unfold State.Valid at h_valid
+    simp [h_valid]
+
+    unfold State.constraintsInVar
+
+    MLIR_states_updates
+    aesop
+  case mp => sorry
 
 end Risc0.IsZero.Constraints.WP
 
