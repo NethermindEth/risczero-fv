a899ed06e02ca9d6893cb5985869148279d0f8d5
ElijahVlasov <elijah.vlasov@gmail.com>
2023-06-26 10:03:09 +0100

    WIP

--- Risc0/Gadgets/OneHot/Witness/WeakestPreFull.lean
+++ Risc0/Gadgets/OneHot/Witness/WeakestPreFull.lean
@@ -4,16 +4,28 @@ import Risc0.Gadgets.OneHot.Witness.WeakestPresPart8
 
 namespace Risc0.OneHot.Witness.WP
 
-def start_state (input: Felt) (st: State) :=
-  st.WellFormed ∧
-  ⟨"input"⟩ ∈ st.vars ∧
-  ⟨"output"⟩ ∈ st.vars ∧
-  -- State.bufferWidths st { name := "input" } = some 1 ∧
-  -- st.bufferWidths[(⟨"output"⟩ : BufferVar)] = some 3 ∧
-  -- (State.buffers st (⟨"input"⟩ : BufferVar)).isSome = true ∧
-  -- (State.buffers st (⟨"input"⟩ : BufferVar)).get!.last! = [some input] ∧
-  -- List.get! (State.buffers st (⟨"input"⟩ : BufferVar)).get! st.cycle = [some input] ∧
-  (st.buffers[(⟨"output"⟩ : BufferVar)]).get!.last! = [none, none, none]
+def start_state (input: Felt) : State :=
+  { buffers := Map.fromList [(⟨"input"⟩, [[.some input]]), (⟨"output"⟩, [[none, none, none]])]
+  , bufferWidths := Map.fromList [(⟨"input"⟩, 1), (⟨"output"⟩, 3)]
+  , constraints := []
+  , cycle := 0
+  , felts := Map.empty
+  , props := Map.empty
+  , vars := [⟨"input"⟩, ⟨"output"⟩]
+  , isFailed := false
+  }
+
+
+-- def start_state (input: Felt) (st: State) :=
+--   st.WellFormed ∧
+--   ⟨"input"⟩ ∈ st.vars ∧
+--   ⟨"output"⟩ ∈ st.vars ∧
+--   State.bufferWidths st { name := "input" } = some 1 ∧
+--   st.bufferWidths[(⟨"output"⟩ : BufferVar)] = some 3 ∧
+--   (State.buffers st (⟨"input"⟩ : BufferVar)).isSome = true ∧
+--   (State.buffers st (⟨"input"⟩ : BufferVar)).get!.last! = [some input] ∧
+--   List.get! (State.buffers st (⟨"input"⟩ : BufferVar)).get! st.cycle = [some input] ∧
+--   (st.buffers[(⟨"output"⟩ : BufferVar)]).get!.last! = [none, none, none]
   -- (st.buffers[(⟨"output"⟩ : BufferVar)]).get
 
 -- lemma temp (h: st.bufferWidths[({ name := "input" } : BufferVar)]! = some 1) : State.bufferWidths st { name := "input" } = some 1 := by
@@ -55,11 +67,9 @@ lemma get_from_pre (input : Felt) (st: State) (h_pre: start_state input st) :
   
 
 lemma closed_form {st: State} {input: Felt} {output₀ output₁ output₂ : Option Felt} :
-  start_state input st → (
-    Code.run st = [output₀, output₁, output₂] ↔ sorry
+  (
+    Code.run (start_state input) = [output₀, output₁, output₂] ↔ sorry
   ) := by
-  intro h_pre
-  unfold start_state at h_pre
   rewrite [part₀_wp]
   rewrite [part₁_updates_opaque]
   rewrite [part₂_updates_opaque]
@@ -69,26 +79,37 @@ lemma closed_form {st: State} {input: Felt} {output₀ output₁ output₂ : Opt
   rewrite [part₆_updates_opaque]
   rewrite [part₇_updates_opaque]
   rewrite [part₈_updates_opaque]
+  unfold start_state
 
   unfold part₀_state
   MLIR_states_updates
-  simp [zero_le, ge_iff_le, forall_true_left, true_and, h_pre, Map.getElem_def, Buffer.Idx.time, Buffer.Idx.data, Back.toNat]
-  sorry
+  simp [zero_le, ge_iff_le, forall_true_left, true_and, Map.getElem_def, Buffer.Idx.time, Buffer.Idx.data, Back.toNat]
+  
+  unfold part₁_state
+  MLIR_states_updates
+  simp [zero_le, ge_iff_le, forall_true_left, true_and, Map.getElem_def, Buffer.Idx.time, Buffer.Idx.data, Back.toNat]
 
+  by_cases eq: input = 0
+  subst eq
+  simp only [ite_true, zero_sub, ite_false]
 
-  -- unfold part₁_state
-  -- MLIR_states_updates
+  -- -- simp only [forall_true_left, ge_iff_le]
+  -- -- simp
 
-  -- unfold part₂_state
-  -- MLIR_states_updates
+  unfold part₂_state
+  simp only [State.updateFelts_felts, Map.update_get!, Option.get!_of_some]
+  MLIR_states_updates
+  -- -- 
+  -- -- subst eq
 
-  -- unfold part₃_state
-  -- MLIR_states_updates
+  unfold part₃_state
+  -- simp [zero_le, ge_iff_le, forall_true_left, true_and, Map.getElem_def, Buffer.Idx.time, Buffer.Idx.data, Back.toNat]
+  MLIR_states_updates
 
-  -- unfold part₄_state
-  -- MLIR_states_updates
+  unfold part₄_state
+  MLIR_states_updates
 
-  -- unfold part₅_state_update
+  -- unfold part₅_state
   -- MLIR_states_updates
 
   -- simp [State.lastOutput, Option.get!, List.getLast!, List.getLast, State.buffers]
