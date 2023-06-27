984d635770f30477374913fc04d4ba0085f7c451
Dom Henderson <dominic@nethermind.io>
2023-06-27 21:47:25 +0100

    WIP

--- Risc0/Map.lean
+++ Risc0/Map.lean
@@ -70,6 +70,20 @@ lemma fromList_nil : fromList ([] : List (α × β)) = Map.empty := rfl
 lemma fromList_cons {l : List (α × β)} :
   fromList ((k, v) :: l) = (Map.fromList l)[k] ←ₘ v := rfl
 
+lemma update_neq_comm (h : k ≠ k') : ((m[k] ←ₘ v)[k'] ←ₘ v') = ((m[k'] ←ₘ v')[k] ←ₘ v) := by
+  simp [update]
+  funext x
+  by_cases eq: x = k
+  subst eq
+  simp
+  aesop
+  simp only
+  by_cases eq': x = k'
+  subst eq'
+  simp only [ite_true]
+  aesop
+  aesop
+
 @[simp]
 lemma update_get : (m[k] ←ₘ v)[k] = v := by simp [update, getElem_def]
 
