89740f414c1ef3283bfeeb9b751b9474d460c6f5
Ferinko <silvasi.frantisek@gmail.com>
2023-06-28 23:48:34 +0200

    Assign <arith> done

--- Risc0/Basic.lean
+++ Risc0/Basic.lean
@@ -566,6 +566,14 @@ end MLIRNotation
 
 instance : Inhabited Felt := ⟨-42⟩
 
+-- Naughty
+@[simp]
+lemma State.get_dropFelts_of_ne {st : State} {x : FeltVar} (h : name ≠ x) :
+  Option.get! (st.dropFelts name).felts[x] = Option.get! st.felts[x] := by
+  unfold State.dropFelts Map.drop
+  rw [Map.getElem_def]
+  aesop
+
 def BufferAtTime.slice (buf : BufferAtTime) (offset size : ℕ) : BufferAtTime :=
   buf.drop offset |>.take size
 
@@ -709,22 +717,28 @@ lemma eval_add : Γ st ⟦@Add α x y⟧ₑ = .some (.Val ((st.felts x).get! + (
 lemma eval_sub : Γ st ⟦@Sub α x y⟧ₑ = .some (.Val ((st.felts x).get! - (st.felts y).get!)) := rfl
 
 @[simp]
+lemma eval_neg : Γ st ⟦@Neg α x⟧ₑ = .some (.Val (0 - (st.felts x).get!)) := rfl
+
+@[simp]
 lemma eval_mul : Γ st ⟦@Mul α x y⟧ₑ = .some (.Val ((st.felts x).get! * (st.felts y).get!)) := rfl
 
 @[simp]
 lemma eval_isz : Γ st ⟦??₀x⟧ₑ = .some (.Val (if (st.felts x).get! = 0 then 1 else 0)) := rfl
 
 @[simp]
-lemma eval_inv : Γ st ⟦Inv x⟧ₑ = .some (.Val (if st.felts[x]!.get! = 0 then 0 else st.felts[x]!.get!⁻¹)) := rfl
+lemma eval_inv : Γ st ⟦Inv x⟧ₑ = .some (.Val (if st.felts[x].get! = 0 then 0 else st.felts[x].get!⁻¹)) := rfl
 
 @[simp]
 lemma eval_andEqz : Γ st ⟦@AndEqz α c x⟧ₑ =
                     .some (.Constraint ((st.props c).get! ∧ (st.felts x).get! = 0)) := rfl
 
 @[simp]
+lemma eval_pow : Γ st ⟦@Pow α x n⟧ₑ = .some (.Val (st.felts[x].get! ^ n)) := rfl
+
+@[simp]
 lemma eval_bitAnd :
   Γ st ⟦@BitAnd α x y⟧ₑ =
-    (.some <| .Val <| feltBitAnd (st.felts x).get! (st.felts y).get!) := rfl
+    (.some <| .Val <| feltBitAnd (st.felts[x]).get! (st.felts[y]).get!) := rfl
 
 @[simp]
 lemma eval_andCond :
--- Risc0/SwapLemmas.lean
+++ Risc0/SwapLemmas.lean
@@ -22,27 +22,84 @@ lemma drop_assign_const_swap {α : IsNondet} {name name' : String} {x : Felt}
 lemma drop_assign_add_swap
   {lhs rhs : FeltVar}
   {α : IsNondet} {name name' : String}
-  (h : name ≠ name') :
+  (h : name ≠ name')
+  (h₁ : ⟨name'⟩ ≠ lhs) 
+  (h₂ : ⟨name'⟩ ≠ rhs) :
   Γ st ⟦name ←ₐ .Add lhs rhs; @MLIR.DropFelt α ⟨name'⟩⟧ =
   Γ st ⟦@MLIR.DropFelt α ⟨name'⟩; name ←ₐ .Add lhs rhs⟧ := by
   MLIR
   simp [State.drop_update_swap, h]
-  generalize eq₁ :
-    ((State.dropFelts st ({ name := name' } : FeltVar))[felts][
-      ({ name := name } : FeltVar)] ← (Option.get! (State.felts st lhs) + Option.get! (State.felts st rhs))) = l
-  
-  -- unfold State.dropFelts Map.drop State.updateFelts
-  -- aesop
-  -- funext y
-  -- unfold Option.get! Map.update
-  -- aesop
+  unfold State.dropFelts Map.drop State.updateFelts
+  aesop
 
--- | Add : FeltVar → FeltVar → Op x
---   | Sub : FeltVar → FeltVar → Op x
---   | Neg : FeltVar           → Op x
---   | Mul : FeltVar → FeltVar → Op x
---   | Pow : FeltVar → ℕ       → Op x
---   | BitAnd : FeltVar → FeltVar → Op x
---   | Inv : FeltVar           → Op InNondet
+lemma drop_assign_sub_swap
+  {lhs rhs : FeltVar}
+  {α : IsNondet} {name name' : String}
+  (h : name ≠ name')
+  (h₁ : ⟨name'⟩ ≠ lhs) 
+  (h₂ : ⟨name'⟩ ≠ rhs) :
+  Γ st ⟦name ←ₐ .Sub lhs rhs; @MLIR.DropFelt α ⟨name'⟩⟧ =
+  Γ st ⟦@MLIR.DropFelt α ⟨name'⟩; name ←ₐ .Sub lhs rhs⟧ := by
+  MLIR
+  simp [State.drop_update_swap, h]
+  unfold State.dropFelts Map.drop State.updateFelts
+  aesop
+
+lemma drop_assign_neg_swap
+  {x : FeltVar}
+  {α : IsNondet} {name name' : String}
+  (h : name ≠ name')
+  (h₁ : ⟨name'⟩ ≠ x) :
+  Γ st ⟦name ←ₐ .Neg x; @MLIR.DropFelt α ⟨name'⟩⟧ =
+  Γ st ⟦@MLIR.DropFelt α ⟨name'⟩; name ←ₐ .Neg x⟧ := by
+  MLIR
+  simp [State.drop_update_swap, h]
+  unfold State.dropFelts Map.drop
+  aesop
+
+lemma drop_assign_mul_swap
+  {lhs rhs : FeltVar}
+  {α : IsNondet} {name name' : String}
+  (h : name ≠ name')
+  (h₁ : ⟨name'⟩ ≠ lhs) 
+  (h₂ : ⟨name'⟩ ≠ rhs) :
+  Γ st ⟦name ←ₐ .Mul lhs rhs; @MLIR.DropFelt α ⟨name'⟩⟧ =
+  Γ st ⟦@MLIR.DropFelt α ⟨name'⟩; name ←ₐ .Mul lhs rhs⟧ := by
+  MLIR
+  simp [State.drop_update_swap, h]
+  unfold State.dropFelts Map.drop State.updateFelts
+  aesop
+
+lemma drop_assign_pow_swap
+  {x : FeltVar} {n : ℕ}
+  {α : IsNondet} {name name' : String}
+  (h : name ≠ name')
+  (h₁ : ⟨name'⟩ ≠ x) :
+  Γ st ⟦name ←ₐ .Pow x n; @MLIR.DropFelt α ⟨name'⟩⟧ =
+  Γ st ⟦@MLIR.DropFelt α ⟨name'⟩; name ←ₐ .Pow x n⟧ := by
+  MLIR
+  simp [State.drop_update_swap, h, State.get_dropFelts_of_ne h₁]
+
+lemma drop_assign_bitand_swap
+  {lhs rhs : FeltVar}
+  {α : IsNondet} {name name' : String}
+  (h : name ≠ name')
+  (h₁ : ⟨name'⟩ ≠ lhs) 
+  (h₂ : ⟨name'⟩ ≠ rhs) :
+  Γ st ⟦name ←ₐ .BitAnd lhs rhs; @MLIR.DropFelt α ⟨name'⟩⟧ =
+  Γ st ⟦@MLIR.DropFelt α ⟨name'⟩; name ←ₐ .BitAnd lhs rhs⟧ := by
+  MLIR
+  simp [State.drop_update_swap, h, feltBitAnd, State.get_dropFelts_of_ne h₁]
+  aesop
+
+lemma drop_assign_inv_swap
+  {x : FeltVar}
+  {α : IsNondet} {name name' : String}
+  (h : name ≠ name')
+  (h₁ : ⟨name'⟩ ≠ x) :
+  Γ st ⟦name ←ₐ .Inv x; dropfelt ⟨name'⟩⟧ =
+  Γ st ⟦dropfelt ⟨name'⟩; name ←ₐ .Inv x⟧ := by
+  MLIR
+  simp [State.drop_update_swap, h, State.get_dropFelts_of_ne h₁]
 
 end Risc0
