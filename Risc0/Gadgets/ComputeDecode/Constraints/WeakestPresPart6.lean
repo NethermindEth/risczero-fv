243f0bd6fa7c6321d972c5c24daf01b876280005
Ferinko <silvasi.frantisek@gmail.com>
2023-07-10 13:10:04 +0200

    WIP

--- Risc0/Gadgets/ComputeDecode/Constraints/WeakestPresPart6.lean
+++ Risc0/Gadgets/ComputeDecode/Constraints/WeakestPresPart6.lean
@@ -304,6 +304,8 @@ lemma part6_cumulative_wp {x0 y0 y1 y2 y3 y4 y5 y6 y7 y8 y9 y10 y11 y12 y13 y14 
     rewrite [part6_updates_opaque]
     unfold part5_state
     MLIR_states_updates
+    
+    
     -- 0 withEqZeros
     -- rewrite [withEqZero_def]
     -- MLIR_states_updates
